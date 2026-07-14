d420b7ffd36bf3897d302a94b7c0c9381a51dff0
fbdusdn12 <fbdusdn12@gmail.com>
2026-07-14 10:03:57 +0900

    chore: init spring boot backend project

---
+++ src/main/java/com/ohdomi/backend/global/TestController.java
@@ -0,0 +1,13 @@
+package com.ohdomi.backend.global;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "OhDomi backend server is running!";
+    }
+}
